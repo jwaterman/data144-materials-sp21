a2d1ee2cd9b352a28e780b08e87990034f4b75a4
Jason Waterman <jwaterman@vassar.edu>
2021-03-17 22:18:11 -0400

    Push homework hw04 to the class

---
+++ hw/hw04/tests/q3_1.py
@@ -0,0 +1,26 @@
+test = {
+  'name': 'Question 3_1',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> player_names.num_columns
+          2
+          >>> player_names.num_rows
+          5
+          >>> player_names.labels[1]
+          'name'
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
