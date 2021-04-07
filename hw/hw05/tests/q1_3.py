de7c986372957357e1ade7645701c98a77dba424
Jason Wateramn <jawaterman@vassar.edu>
2021-04-07 18:19:18 -0400

    Push homework hw05 to the class

---
+++ hw/hw05/tests/q1_3.py
@@ -0,0 +1,22 @@
+test = {
+    'name': 'Question 1_3',
+    'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> 1 <= eleventh_roll <= 3
+          True
+          """,
+            'hidden': False,
+          'locked': False
+        }
+      ],
+        'scored': True,
+        'setup': '',
+        'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
