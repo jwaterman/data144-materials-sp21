d6512cdf50a879b84eba104611265d2bcc5a34d0
Jason Wateramn <jawaterman@vassar.edu>
2021-05-07 10:28:25 -0400

    Push lab lab11 to the class

---
+++ lab/lab11/tests/q2_4.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q2_4',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> set(q2_4) == set([1, 3, 5, 7])
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
