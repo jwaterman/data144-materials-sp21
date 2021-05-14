0bbaf27fc028cee31caa8c5c4de65c922aa6d9f8
Jason Wateramn <jawaterman@vassar.edu>
2021-05-14 05:03:16 -0400

    Push lab lab12 to the class

---
+++ lab/lab12/tests/q2_6.py
@@ -0,0 +1,22 @@
+test = {
+  'name': 'q2_6',
+  'points': 1,
+  'suites': [
+    {
+      'cases': [
+        {
+          'code': r"""
+          >>> np.isclose(mystery_correlation, -0.024711531764254033)
+          True
+          """,
+          'hidden': False,
+          'locked': False
+        }
+      ],
+      'scored': True,
+      'setup': '',
+      'teardown': '',
+      'type': 'doctest'
+    }
+  ]
+}
